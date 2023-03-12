65962ec6956e6081add57c515773146ecaf8fbaa
James Demiraiakian <james.armen@gmail.com>
2023-03-11 16:36:03 -0800

    wrote out fetch by user_id and tournament_id routes

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, tournaments;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   username TEXT,
   email TEXT NOT NULL UNIQUE
 );
+
+CREATE TABLE tournaments (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  date DATE,
+  user_id TEXT,
+  fencerCount INT,
+  fencerArr TEXT[]
+)
